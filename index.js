cde8366ea05d394b5a446e9b2e55a2994ae91bc5
Eduardo <eduardomelle@gmail.com>
2022-08-21 08:22:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, resp) => {
+  resp.send('Hello World com Node.js com Docker!');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
+});
