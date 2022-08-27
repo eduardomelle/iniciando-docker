b69d4b01ebad391511a9aa8d76b9f5904f424c3c
Eduardo <eduardomelle@gmail.com>
2022-08-27 14:19:26 -0300

    Docker Compose: Ambiente de desenvolvimento para aplicações de grande porte

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,6 @@ FROM node:14.17.0-slim
 
 WORKDIR /home/node/app
 
+USER node
+
 CMD ["tail", "-f", "/dev/null"]
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, resp) => {
-  resp.send('Hello World com Node.js com Docker!');
+  resp.send('Hello World with Node.js and Docker Compose!');
 });
 
 app.listen(process.env.PORT || 3000, () => {
